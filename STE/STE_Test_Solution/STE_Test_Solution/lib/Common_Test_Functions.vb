9242d20a9f3f4a17c1b236dd917b230adc45ef5f
Bryson Noble <noblebryson@gmail.com>
2026-04-17 01:23:58 -0400

    Update Example_Test and begin testlib

---
+++ STE/STE_Test_Solution/STE_Test_Solution/lib/Common_Test_Functions.vb
@@ -0,0 +1,9 @@
+﻿Module Common_Test_Functions
+    Function BeginTest()
+
+    End Function
+
+    Function EndTest()
+
+    End Function
+End Module
--- Test Scripts/Example_Test.vb
+++ Test Scripts/Example_Test.vb
@@ -1,7 +1,23 @@
 Imports System
 
 Module Example_Test
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+    Sub Main()
+        BeginTest()
+
+        TC01()
+        TC02()
+        TC03()
+
+        EndTest()
+    End Sub
+
+    Sub TC01()
+        ' Test Case 01
+    End Sub
+    Sub TC02()
+        ' Test Case 02
+    End Sub
+    Sub TC03()
+        ' Test Case 03
     End Sub
 End Module
