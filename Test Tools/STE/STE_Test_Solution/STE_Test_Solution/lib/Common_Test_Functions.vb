378ea5a87a0f358c435390388fc1223e7f5b0293
Bryson Noble <noblebryson@gmail.com>
2026-04-21 13:41:38 -0400

    Update Common_Test_Functions.vb

--- Test Tools/STE/STE_Test_Solution/STE_Test_Solution/lib/Common_Test_Functions.vb
+++ Test Tools/STE/STE_Test_Solution/STE_Test_Solution/lib/Common_Test_Functions.vb
@@ -1,10 +1,16 @@
-﻿Module Common_Test_Functions
-    Function BeginTest()
+﻿Imports System.IO
 
+Module Common_Test_Functions
+    Function BeginTest()
+        ' TODO: launch all programs
     End Function
 
     Function EndTest()
+        ' TODO: close all programs
 
+        ' Clear the shared .txt files used in the Unity scene to prevent test data from being used in future tests.
+        File.WriteAllText("..\..\SHIELD Virtual Camera\Assets\STE Shared Data\TargetList.txt", "")
+        File.WriteAllText("..\..\SHIELD Virtual Camera\Assets\STE Shared Data\VirtualEnvironment.txt", "")
     End Function
 
     ' This function Is used to edit the virtual environment "setting" for the test
